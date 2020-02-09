83f8899e79a0cfef4eec531c41b8e1d69ebb1610
jp6ru8958958 <jp6ru8958958@gmail.com>
2020-02-10 00:47:22 +0800

    image resize

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import logging
 import json
 import urllib.request
 import zipfile
+import cv2
+import numpy as np
+import os
+from os import listdir
+
 
 def start(update, context):
 	print(update.effective_chat.username)
-	context.bot.send_message(chat_id=update.effective_chat.id, text="Just give me the sticker's store link!\nExample: https://store.line.me/stickershop/product/8751482/zh-Hant")
+	context.bot.send_message(chat_id=update.effective_chat.id, text='Just give me the sticker\'s store link!\nExample: https://store.line.me/stickershop/product/8751482/zh-Hant')
 
 def echo(update, context):
 	if(update.message.text[0:42] == 'https://store.line.me/stickershop/product/'):
@@ -14,23 +19,23 @@ def echo(update, context):
 		Stickers_ID = ''.join(list(filter(str.isdigit, Stickers_ID)))
 		print(Stickers_ID)
 		if(downloader(Stickers_ID)):
-			context.bot.send_message(chat_id=update.effective_chat.id, text="Download "+Stickers_ID+" Success!\nJust wait minute for upload to telegram!")
+			context.bot.send_message(chat_id=update.effective_chat.id, text='Download '+Stickers_ID+' Success!\nJust wait minute for upload to telegram!')
 			image_transcoding()
 		else:
-			context.bot.send_message(chat_id=update.effective_chat.id, text="Something error!\nPlease try other sticker link.")
+			print('Download error.')
+			context.bot.send_message(chat_id=update.effective_chat.id, text='Something error!\nPlease try other sticker link.')
 	else:
-		context.bot.send_message(chat_id=update.effective_chat.id, text="Wrong input content!\nPlease give me the sticker's store link.\nExample: https://store.line.me/stickershop/product/00001/zh-Hant")
+		context.bot.send_message(chat_id=update.effective_chat.id, text='Wrong input content!\nPlease give me the sticker\'s store link.\nExample: https://store.line.me/stickershop/product/00001/zh-Hant')
 
 def downloader(Stickers_ID):
-	dl_pkg_link = "http://dl.stickershop.line.naver.jp/products/0/0/1/"+Stickers_ID+"/iphone/stickers@2x.zip"
+	dl_pkg_link = 'http://dl.stickershop.line.naver.jp/products/0/0/1/'+Stickers_ID+'/iphone/stickers@2x.zip'
 	try:
 		dl_file = urllib.request.urlopen(dl_pkg_link)
-		with open("Stickers.zip", "wb") as ZipFile:
+		with open('Stickers.zip', 'wb') as ZipFile:
 			ZipFile.write(dl_file.read())
-			print(Stickers_ID + " download finish.")
+			print(Stickers_ID+' download finish.')
 			return True
 	except:
-		print("Download error")
 		return False
 	
 def image_transcoding():
@@ -38,7 +43,23 @@ def image_transcoding():
 		for fileM in zFile.namelist():
 			zFile.extract(fileM, 'Stickers')
 		zFile.close()
-	
+	img_files = listdir('Stickers')
+	amount=0
+	for img_name in img_files:
+		if 'key' not in img_name and 'tab' not in img_name and 'pro' not in img_name:
+			amount+=1
+			print(img_name+' finish resize ', amount)
+			image=cv2.imread('Stickers/'+img_name, cv2.IMREAD_UNCHANGED)
+			if image.shape[0]>=image.shape[1]:
+				mag = 512/image.shape[0]
+				src=cv2.resize(image, ( int(image.shape[1]*mag), 512), interpolation=cv2.INTER_CUBIC)
+				src[ 63, 63, :] = [ 255, 0, 0, 128]
+				cv2.imwrite('image'+str(amount)+'.png', src, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
+			else:
+				mag = 512/image.shape[1]
+				src=cv2.resize(image, ( 512, int(image.shape[0]*mag)), interpolation=cv2.INTER_CUBIC)
+				src[ 63, 63, :] = [ 255, 0, 0, 128]
+				cv2.imwrite('image'+str(amount)+'.png', src, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
 
 def main():
 	TOKEN = json.load(open('..//..//Api_Token.json', 'r'))
