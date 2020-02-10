ca45cbfc2372f3abe838ed5b6d7afdc7e760a8d7
jp6ru8958958 <jp6ru8958958@gmail.com>
2020-02-10 15:54:36 +0800

    upload finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
 import logging
 import json
 import urllib.request
@@ -7,6 +8,9 @@ import cv2
 import numpy as np
 import os
 from os import listdir
+import emoji
+from emoji import emojize
+import shutil
 
 
 def start(update, context):
@@ -21,6 +25,7 @@ def echo(update, context):
 		if(downloader(Stickers_ID)):
 			context.bot.send_message(chat_id=update.effective_chat.id, text='Download '+Stickers_ID+' Success!\nJust wait minute for upload to telegram!')
 			image_transcoding()
+			upload_to_telegram(update, context)
 		else:
 			print('Download error.')
 			context.bot.send_message(chat_id=update.effective_chat.id, text='Something error!\nPlease try other sticker link.')
@@ -44,22 +49,51 @@ def image_transcoding():
 			zFile.extract(fileM, 'Stickers')
 		zFile.close()
 	img_files = listdir('Stickers')
-	amount=0
+	amount = 0
 	for img_name in img_files:
 		if 'key' not in img_name and 'tab' not in img_name and 'pro' not in img_name:
-			amount+=1
-			print(img_name+' finish resize ', amount)
-			image=cv2.imread('Stickers/'+img_name, cv2.IMREAD_UNCHANGED)
+			amount += 1		
+			image = cv2.imread('Stickers/'+img_name, cv2.IMREAD_UNCHANGED)
 			if image.shape[0]>=image.shape[1]:
 				mag = 512/image.shape[0]
-				src=cv2.resize(image, ( int(image.shape[1]*mag), 512), interpolation=cv2.INTER_CUBIC)
+				src = cv2.resize(image, ( int(image.shape[1]*mag), 512), interpolation=cv2.INTER_CUBIC)
 				src[ 63, 63, :] = [ 255, 0, 0, 128]
 				cv2.imwrite('image'+str(amount)+'.png', src, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
 			else:
 				mag = 512/image.shape[1]
-				src=cv2.resize(image, ( 512, int(image.shape[0]*mag)), interpolation=cv2.INTER_CUBIC)
+				src = cv2.resize(image, ( 512, int(image.shape[0]*mag)), interpolation=cv2.INTER_CUBIC)
 				src[ 63, 63, :] = [ 255, 0, 0, 128]
 				cv2.imwrite('image'+str(amount)+'.png', src, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
+			print(img_name+' finish resize ', amount)
+	print('All image resize finish!')
+
+def upload_to_telegram(update, context):
+	sticker_info = json.load(open(f'Stickers//productInfo.meta'))
+	eng_name = sticker_info['title']['en']
+	stk_id = sticker_info['packageId']
+	if 'zh-Hant' in sticker_info['title']:
+		tw_name = sticker_info['title']['zh-Hant']
+	else:
+		tw_name = eng_name
+	sticker_name = 'id'+str(stk_id)+'_by_TLStT_bot'
+	emoj = emoji.emojize(':smile:', use_aliases=True)
+	print(update.message.from_user.id, sticker_name, tw_name, eng_name)
+	amount = 0
+	img_file = listdir()
+	for png_sticker in img_file:
+		if '.png' in png_sticker:
+			amount+=1
+			try:
+				context.bot.addStickerToSet( update.message.from_user.id, sticker_name, open(f''+png_sticker, 'rb'), emoj)
+			except telegram.error.BadRequest:
+				context.bot.createNewStickerSet( update.message.from_user.id, sticker_name, eng_name, open( f''+png_sticker, 'rb'), emoj)
+			os.remove(png_sticker)
+			print('upload '+sticker_name+' success. ', amount)
+	shutil.rmtree('Stickers')
+	os.remove('Stickers.zip')
+	print('finish!\nhttps://t.me/addstickers/'+sticker_name)
+	context.bot.send_message(chat_id=update.effective_chat.id, text='https://t.me/addstickers/'+sticker_name)
+
 
 def main():
 	TOKEN = json.load(open('..//..//Api_Token.json', 'r'))
