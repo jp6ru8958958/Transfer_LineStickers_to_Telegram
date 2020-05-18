c813c4f89a5dcda7bc1ab055e32ea938ffbe8a16
jp6ru8958958 <jp6ru8958958@gmail.com>
2020-05-18 14:30:38 +0800

    beautify

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import json
 import urllib.request
 import zipfile
 import cv2
-import numpy as np
 import os
 from os import listdir
 import emoji
@@ -14,27 +13,31 @@ import shutil
 
 
 class bot():
-    def start(update, context):
-        print('{{/start}}:({username})'.format(username=update.effective_chat.username))
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Just give me the sticker\'s store link!\nExample: https://store.line.me/stickershop/product/8751482/zh-Hant')
+    def start(updater, context):
+        print('{{/start}}:({username})'.format(username=updater.effective_chat.username))
+        context.bot.send_message(
+            chat_id=updater.effective_chat.id,
+            text='Just give me the sticker\'s store link!\nExample: https://store.line.me/stickershop/product/8751482/zh-Hant')
 
-    def echo(update, context):
-        print('{{/echo}}:({username}):{text}'.format(username=update.effective_chat.username, text=update.message.text))
-        if update.message.text[0:42] == 'https://store.line.me/stickershop/product/':
-            Sticker_set = sticker_set_process(update, context)
-            Sticker_set.ID = update.message.text[42::]
+    def echo(updater, context):
+        print('{{/echo}}:({username}):{text}'.format(username=updater.effective_chat.username, text=updater.message.text))
+        if updater.message.text[0:42] == 'https://store.line.me/stickershop/product/':
+            Sticker_set = sticker_set_process(updater, context)
+            Sticker_set.ID = updater.message.text[42::]
             Sticker_set.ID = ''.join(list(filter(str.isdigit, Sticker_set.ID)))
             print(Sticker_set.ID)
             Sticker_set.get_sticker_set()
             Sticker_set.image_transcoding()
             Sticker_set.upload_to_telegram()
         else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Wrong input content!\nPlease give me the sticker\'s store link.\nExample: https://store.line.me/stickershop/product/00001/zh-Hant')
+            context.bot.send_message(
+                chat_id = updater.effective_chat.id, 
+                text = 'Wrong input content!\nPlease give me the sticker\'s store link.\nExample: https://store.line.me/stickershop/product/00001/zh-Hant')
+
 
 class sticker_set_process():
-    def __init__(self, update, context):
-        self.ID = ''
-        self.update = update
+    def __init__(self, updater, context):
+        self.updater = updater
         self.context = context
 
     def get_sticker_set(self):
@@ -46,11 +49,14 @@ class sticker_set_process():
             with open('Stickers.zip', 'wb') as ZipFile:
                 ZipFile.write(dl_file.read())
                 print(self.ID+' download finish.')
-                self.context.bot.send_message(chat_id=self.update.effective_chat.id, text='Download '+Sticker_set.ID+' Success!\nJust wait minute for upload to telegram!')
+                self.context.bot.send_message(
+                    chat_id = self.updater.effective_chat.id,
+                    text = 'Download '+Sticker_set.ID+' Success!\nJust wait minute for upload to telegram!')
                 return True
         except:
+            print('Download sticker set error.')
             return False
-        
+
     def image_transcoding(self):
         # Transcoding line's sticker as telegram's spcification.
         with zipfile.ZipFile('Stickers.zip', 'r') as zFile:
@@ -79,7 +85,7 @@ class sticker_set_process():
                     cv2.imwrite('image'+str(amount)+'.png', src, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
                 print(img_name+' finish resize ', amount)
         print('All image resize finish!')
-        
+
     def upload_to_telegram(self):
         sticker_info = json.load(open('Stickers//productInfo.meta'))
         self.eng_name = sticker_info['title']['en']
@@ -88,24 +94,35 @@ class sticker_set_process():
         else:
             self.tw_name = self.eng_name
         self.tg_sticker_set_name = 'id_'+str(self.ID)+'_by_TLStT_bot'
-        emj = emoji.emojize(':smile:', use_aliases=True)
+        default_emj = emoji.emojize(':smile:', use_aliases=True)
         # Get an emoji for upload need.
-        print(self.update.message.from_user.id, self.tg_sticker_set_name, self.tw_name, self.eng_name)
+        print(self.updater.message.from_user.id, self.tg_sticker_set_name, self.tw_name, self.eng_name)
         amount = 0
         img_file = listdir()
         for png_sticker in img_file:
             if '.png' in png_sticker:
                 amount+=1
                 try:
-                    self.context.bot.addStickerToSet(self.update.message.from_user.id, self.tg_sticker_set_name, emj, open(''+png_sticker, 'rb'))
+                    self.context.bot.addStickerToSet(
+                        self.updater.message.from_user.id, 
+                        self.tg_sticker_set_name, 
+                        default_emj, 
+                        open(''+png_sticker, 'rb'))
                 except telegram.error.BadRequest:
-                    self.context.bot.createNewStickerSet(self.update.message.from_user.id, self.tg_sticker_set_name, self.eng_name, emj, open(''+png_sticker, 'rb'))
+                    self.context.bot.createNewStickerSet(
+                        self.updater.message.from_user.id, 
+                        self.tg_sticker_set_name, 
+                        self.eng_name, 
+                        default_emj, 
+                        open(''+png_sticker, 'rb'))
                 os.remove(png_sticker)
                 print('upload '+self.tg_sticker_set_name+' success. ', amount)
         shutil.rmtree('Stickers')
         os.remove('Stickers.zip')
         print('finish!\nhttps://t.me/addstickers/'+self.tg_sticker_set_name)
-        self.context.bot.send_message(chat_id=self.update.effective_chat.id, text=self.tw_name+'\nhttps://t.me/addstickers/'+self.tg_sticker_set_name)
+        self.context.bot.send_message(
+            chat_id=self.updater.effective_chat.id, 
+            text=self.tw_name+'\nhttps://t.me/addstickers/'+self.tg_sticker_set_name)
     
     def record_converted_stickers(self):
         jsonFile = open('converted_stickers.json', 'r')
@@ -118,22 +135,24 @@ class sticker_set_process():
     def checkExist(self):
         return 0
 
-def reset_env():
 
-
-
-    pass
-
-def bot_setting():
-    TOKEN = json.load(open('..//..//Api_Token.json', 'r'))
-    updater = Updater(token=TOKEN['Telegram']['Transfer_LineStickers_to_Telegram'], use_context=True)
-    dispatcher = updater.dispatcher
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def commands(dispatcher, updater):
     start_handler = CommandHandler('start', bot.start)
     echo_handler = MessageHandler(Filters.text, bot.echo)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(echo_handler)
     updater.start_polling()
+
+def bot_setting():
+    TOKEN = json.load(open('..//..//Api_Token.json', 'r'))
+    updater = Updater(
+        token=TOKEN['Telegram']['Transfer_LineStickers_to_Telegram'],
+        use_context=True)
+    dispatcher = updater.dispatcher
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+    commands(dispatcher, updater)
 
 
 if __name__=="__main__":
